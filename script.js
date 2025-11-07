51bed06952a62183f3391b318a8acf266757a69d
thiagoN007 <gitthiago9@gmail.com>
2025-11-07 18:06:21 -0300

    app de tempo atualizado 2.1

--- script.js
+++ script.js
@@ -17,16 +17,14 @@ async function buscar() {
         console.log(dado)
         document.querySelector('.cidade').innerHTML = `${dado.name}`;
         document.querySelector('.graus').innerHTML = `Temperatura: ${dado.main.temp.toFixed(0)}°C`;
-        document.querySelector('.grau_max').innerHTML =
-        `Temperatura Máx: ${dado.main.temp_max.toFixed(0)}°C`;
-        document.querySelector('.umidade').innerHTML = `Umidade: ${dado.main.humidity} %`; 
-        document.querySelector('.tempo').innerHTML = `Tempo:${dado.weather[0].description}`;
+        document.querySelector('.umidade').innerHTML = `Umidade: ${dado.main.humidity}%`; 
+        document.querySelector('.tempo').innerHTML = `${dado.weather[0].description}`;
+        
 
     })
     .catch(error => {
         console.error("Erro na requisição da api", error)
     });
 
-    document.getElementById("cidade").value = "";
 }
 
