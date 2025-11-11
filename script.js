70889fd58026f535665d04f267dddf4be279b4d3
thiagoN007 <gitthiago9@gmail.com>
2025-11-10 23:11:31 -0300

    app tempo atualizado: 10/11/2025

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 let key = "eca3250ca768b6721c13c3da57714354";
-async function buscar() {
-    
-    const cidade = document.getElementById("cidade").value;
-
-    if(cidade == "") {
-        document.getElementById("cidade");
-        document.getElementById("cidade").focus();
-        return ;
-    }
-
+async function buscar(cidadePadrao) {
+    let cidade = cidadePadrao || document.getElementById("cidade").value;
     document.getElementById("cidade").value = "";
-
     await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&appid=${key}&units=metric`)
     .then(res => res.json())
     .then(dado => {
         console.log(dado)
         document.querySelector('.cidade').innerHTML = `${dado.name}`;
-        document.querySelector('.graus').innerHTML = `Temperatura: ${dado.main.temp.toFixed(0)}°C`;
-        document.querySelector('.umidade').innerHTML = `Umidade: ${dado.main.humidity}%`; 
-        document.querySelector('.tempo').innerHTML = `${dado.weather[0].description}`;
-        
-
+        document.querySelector('.graus').innerHTML = `${dado.main.temp.toFixed(0)}°C`;
+        document.querySelector('.sensacao').innerHTML = `${dado.main.feels_like.toFixed(0)}°C`;
+        document.querySelector('.umidade').innerHTML = `${dado.main.humidity}%`;
+        document.querySelector('.tempo').innerHTML = `${dado.weather[0].description} <img src='https://openweathermap.org/img/wn/${dado.weather[0].icon}.png'>`;
     })
     .catch(error => {
         console.error("Erro na requisição da api", error)
     });
+}
 
+// buscarcidade padrão ao carregar a página
+
+window.onload = function() {
+    buscar("Uniao");
 }
 
