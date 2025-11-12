9fb2071b9865e09655018c02489feeafe5f28d1e
thiagoN007 <gitthiago9@gmail.com>
2025-11-11 21:38:56 -0300

    Atualizado: 11/11/25 v.3.1

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let key = "eca3250ca768b6721c13c3da57714354";
+let key = "eca3250ca768b6721c13c3da57714354"; //chave da API
 async function buscar(cidadePadrao) {
-    let cidade = cidadePadrao || document.getElementById("cidade").value;
+    let cidade = cidadePadrao || document.getElementById("cidade").value;//pega o valor cidade
     document.getElementById("cidade").value = "";
+    if(cidade == "") { //valida o campo cidade se estiver vazio!
+        alert("Digite o nome da cidade!");
+        return;
+    }
     await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&appid=${key}&units=metric`)
     .then(res => res.json())
     .then(dado => {
@@ -13,11 +17,12 @@ async function buscar(cidadePadrao) {
         document.querySelector('.tempo').innerHTML = `${dado.weather[0].description} <img src='https://openweathermap.org/img/wn/${dado.weather[0].icon}.png'>`;
     })
     .catch(error => {
-        console.error("Erro na requisição da api", error)
+        console.error("Erro na requisição da api", error) //exibe o erro de requisicao
     });
+
 }
 
-// buscarcidade padrão ao carregar a página
+// buscarcidade padrão ao carregar a página Ex.: Uniao, teresina
 
 window.onload = function() {
     buscar("Uniao");
