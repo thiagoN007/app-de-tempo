4667b56104d80b987d42ca374537519ad8b4fd8b
thiagoN007 <gitthiago9@gmail.com>
2025-11-15 21:53:28 -0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ﻿#Tempo agora site/app
 
 Um pequeno projeto integrado com api
-que mostra em tempo real a temperatura de qualquer cidade,buacando apenas pelo nome dela, obtendo informaçoes de temperatura,sensacao térmica e umidade.
+que mostra em tempo real a temperatura de qualquer cidade,buscando apenas pelo nome dela, obtendo informaçoes de temperatura,sensação térmica e umidade.
--- script.js
+++ script.js
@@ -9,12 +9,18 @@ async function buscar(cidadePadrao) {
     await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&appid=${key}&units=metric`)
     .then(res => res.json())
     .then(dado => {
+        if (!dado.main) {
+            alert(`Cidade não encontrada!`);
+            return;
+        }
         console.log(dado)
         document.querySelector('.cidade').innerHTML = `${dado.name}`;
         document.querySelector('.graus').innerHTML = `<p class="p-universal">Temperatura: ${dado.main.temp.toFixed(0)} °C</p>`;
         document.querySelector('.sensacao').innerHTML = `<p class="p-universal">Sensação Térmica: ${dado.main.feels_like = Math.floor(dado.main.feels_like)} °C </p>`;
         document.querySelector('.umidade').innerHTML = `<p class="p-universal">Umidade: ${dado.main.humidity}%</p>`;
-        document.querySelector('.tempo').innerHTML = `<p class="p-universal">${dado.weather[0].description} <img src='https://openweathermap.org/img/wn/${dado.weather[0].icon}.png'></p>`;
+        document.querySelector('.tempo').innerHTML = `<p class="p-universal">${dado.weather[0].description} 
+        <img src='https://openweathermap.org/img/wn/${dado.weather[0].icon}.png'></p>`;
+
     })
     .catch(error => {
         console.error("Erro na requisição da api", error) //exibe o erro de requisicao
