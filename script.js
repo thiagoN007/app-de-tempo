f1d7cb5d28f5b22252e0adbcb1aef45d8d3f3a4d
thiagoN007 <gitthiago9@gmail.com>
2025-11-24 20:58:43 -0300

    ajust de letra

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function buscar(cidadePadrao) {
         document.querySelector('.tempo').innerHTML = `${dado.weather[0].description} 
         <img src='https://openweathermap.org/img/wn/${dado.weather[0].icon}.png'><br>`;
 
-        document.querySelector('.graus').innerHTML = `<p class="temp">Temperatura<br></p>${dado.main.temp.toFixed(0)} °C`;
+        document.querySelector('.graus').innerHTML = `<p class="tempe">Temperatura<br></p>${dado.main.temp.toFixed(0)} °C`;
         document.querySelector('.sensacao').innerHTML = `<p class="sensa">Sensação<br></p>${dado.main.feels_like = Math.floor(dado.main.feels_like)} °C`;
         document.querySelector('.umidade').innerHTML = `<p class="umid">Umidade<br></p>${dado.main.humidity}%`;
         
